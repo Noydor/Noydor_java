954a39a403ca970deee4a0a3d167a459e153896b
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-10-27 02:58:18 +0300

    Тестовый фреймворк и первые тесты

---
+++ sandbox/src/test/java/ru/stqa/noy/sandbox/PointTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.noy.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testDistance() {
+    Point p1 = new Point(1,4);
+    Point p2 = new Point(4,1);
+    Assert.assertEquals( p1.distance(p2), 4.242640687119285);
+  }
+
+}
