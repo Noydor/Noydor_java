72d4ff04f765436f4fe9010856280e762bc2cdb2
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-10-29 15:07:03 +0300

    NewContactTests со вспом. методами, параметризацией и вспом. классом

---
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/AddNewData.java
@@ -0,0 +1,67 @@
+package ru.stqa.noy.addressbook;
+
+public class AddNewData {
+  private final String firstname;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String home;
+  private final String mobile;
+  private final String email;
+  private final String homepage;
+
+  public AddNewData(String firstname, String lastname, String nickname, String title, String company, String address, String home, String mobile, String email, String homepage) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.home = home;
+    this.mobile = mobile;
+    this.email = email;
+    this.homepage = homepage;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHome() {
+    return home;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/NewContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/NewContactTests.java
@@ -5,18 +5,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class NewContactTests {
   FirefoxDriver wd;
@@ -26,50 +18,70 @@ public class NewContactTests {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
   @Test
   public void testNewContact() {
-    wd.findElement(By.linkText("add new")).click();
+    gotoAddNewPage();
+    fillAddNewForm(new AddNewData("Samuel", "Smith", "Smithy", "Mr", "Seastar", "Berlin, Niederkirchnerstrasse, 18", "4955123456", "4976543210", "samuelsmith@gmail.com", "samuelsmith.com"));
+    submitAddNew();
+    returnToHomePage();
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void submitAddNew() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillAddNewForm(AddNewData addNewData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Samuel");
+    wd.findElement(By.name("firstname")).sendKeys(addNewData.getFirstname());
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Smith");
+    wd.findElement(By.name("lastname")).sendKeys(addNewData.getLastname());
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("Smithy");
+    wd.findElement(By.name("nickname")).sendKeys(addNewData.getNickname());
     wd.findElement(By.name("title")).click();
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("Mr");
+    wd.findElement(By.name("title")).sendKeys(addNewData.getTitle());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Seastar");
+    wd.findElement(By.name("company")).sendKeys(addNewData.getCompany());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Berlin, Niederkirchnerstrasse, 18");
+    wd.findElement(By.name("address")).sendKeys(addNewData.getAddress());
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("4955123456");
+    wd.findElement(By.name("home")).sendKeys(addNewData.getHome());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("4976543210");
+    wd.findElement(By.name("mobile")).sendKeys(addNewData.getMobile());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("samuelsmith@gmail.com");
+    wd.findElement(By.name("email")).sendKeys(addNewData.getEmail());
     wd.findElement(By.name("homepage")).click();
     wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys("samuelsmith.com");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("home")).click();
+    wd.findElement(By.name("homepage")).sendKeys(addNewData.getHomepage());
+  }
+
+  private void gotoAddNewPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod
