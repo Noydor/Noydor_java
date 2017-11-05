b1ee9e1f762a1ca5fc1f9fa2c3c905ad05968ddf
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-11-05 18:25:49 +0300

    Общий класс TestBase для GroupCreationTests и GroupDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.noy.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+  @Test
+  public void testGroupDeletion() {
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returnToGroupPage();
+  }
+
+}
