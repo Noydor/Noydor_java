c6e74fa664104a8e40905bfc0c92ad3d56a36d11
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-11-06 03:47:25 +0300

    Добавлен ContactModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.noy.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.noy.addressbook.model.AddNewData;
+
+public class ContactModificationTests extends TestBase{
+
+  @Test
+  public void testContactModification() {
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillAddNewForm(new AddNewData("Samuel", "Smith", "Smithy", "Mr", "Seastar", "Berlin, Niederkirchnerstrasse, 18", "4955123456", "4976543210", "samuelsmith@gmail.com", "samuelsmith.com"));
+    app.getContactHelper().submitContactModification();
+    
+  }
+
+}
