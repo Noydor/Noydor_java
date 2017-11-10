943dac0e19b200ead3375d1af6b3e6f18d17d3d4
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-11-10 23:44:57 +0300

    Сделан общий WebDriver; добавлены условия с браузерами

--- addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/appmanager/ContactHelper.java
@@ -1,13 +1,14 @@
 package ru.stqa.noy.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.noy.addressbook.model.AddNewData;
 
 public class ContactHelper extends HelperBase {
   private FirefoxDriver wd;
 
-  public ContactHelper(FirefoxDriver wd) {
+  public ContactHelper(WebDriver wd) {
     super(wd);
   }
 
