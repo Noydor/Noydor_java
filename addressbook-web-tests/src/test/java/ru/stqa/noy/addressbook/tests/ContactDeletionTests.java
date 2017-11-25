9a7b0076df5765e5fdd0800de2d7c92c74bf31e8
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-11-25 19:40:29 +0300

    Выбор элемента списка при модиф. или удалении контакта

--- addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/tests/ContactDeletionTests.java
@@ -13,7 +13,7 @@ public class ContactDeletionTests extends TestBase{
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new AddNewData("Samuel", "Smith", "Smithy", "Mr", "Seastar", "Berlin, Niederkirchnerstrasse, 18", "4955123456", "4976543210", "samuelsmith@gmail.com", "samuelsmith.com", "test1"), true);
     }
-    app.getContactHelper().selectContact();
+    app.getContactHelper().selectContact(before - 1);
     app.getContactHelper().deleteSelectedContact();
     app.closeAlert();
     app.getNavigationHelper().gotoHomePage();
--- addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/tests/ContactModificationTests.java
@@ -13,6 +13,7 @@ public class ContactModificationTests extends TestBase{
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new AddNewData("Samuel", "Smith", "Smithy", "Mr", "Seastar", "Berlin, Niederkirchnerstrasse, 18", "4955123456", "4976543210", "samuelsmith@gmail.com", "samuelsmith.com", "test1"), true);
     }
+    app.getContactHelper().selectContact(before - 1);
     app.getContactHelper().initContactModification();
     app.getContactHelper().fillAddNewForm(new AddNewData("Samuel", "Smith", "Smithy", "Mr", "Seastar", "Berlin, Niederkirchnerstrasse, 18", "4955123456", "4976543210", "samuelsmith@gmail.com", "samuelsmith.com", null), false);
     app.getContactHelper().submitContactModification();
