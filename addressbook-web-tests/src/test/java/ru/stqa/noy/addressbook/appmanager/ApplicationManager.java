230e75faea3dca8c7d1240fcaee4b3ef0d0726e1
Rodion Antonov <rodion.v.antonov@gmail.com>
2017-12-23 17:23:40 +0300

    (8.3) Взаимодействие с сервером по протоколу HTTP

--- addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/noy/addressbook/appmanager/ApplicationManager.java
@@ -55,7 +55,6 @@ public class ApplicationManager {
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
   }
 
-
   public void stop() {
     wd.quit();
   }
