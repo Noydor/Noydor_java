0a8206cfc1b816d5f1d71119eb91cb10f7a47dee
Rodion Antonov <rodion.v.antonov@gmail.com>
2018-02-10 22:34:40 +0300

    Задание №19. Начало.

--- mantis-tests/src/test/java/ru/stqa/noy/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/noy/mantis/tests/TestBase.java
@@ -1,6 +1,7 @@
 package ru.stqa.noy.mantis.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.noy.mantis.appmanager.ApplicationManager;
@@ -15,12 +16,24 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
-    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
+   /* app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak"); */
   }
 
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws IOException {
-    app.ftp().restore("config_inc.php.bak", "config_inc.php");
+   /* app.ftp().restore("config_inc.php.bak", "config_inc.php"); */
     app.stop();
   }
+
+  boolean isIssueOpen(int issueId) {
+    
+
+  }
+
+  public void skipIfNotFixed(int issueId) {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
 }
