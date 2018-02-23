19493ef146c4bfacde6e4da439e19f6152a53696
Rodion Antonov <rodion.v.antonov@gmail.com>
2018-02-23 20:16:29 +0300

    Задание №20: Интеграция тестов с баг-трекером Bugify

--- rest-sample/src/test/java/ru/stqa/noy/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/noy/rest/RestTests.java
@@ -4,22 +4,20 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
-import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.message.BasicNameValuePair;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase{
 
   @Test
   public void testCreateIssue() throws IOException {
+    skipIfNotFixed(780);
     Set<Issue> oldIssues = getIssues();
     Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
     int issueId = createIssue(newIssue);
@@ -33,10 +31,6 @@ public class RestTests {
     JsonElement parsed = new JsonParser().parse(json);
     JsonElement issues = parsed.getAsJsonObject().get("issues");
     return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
-  }
-
-  private Executor getExecutor() {
-    return Executor.newInstance().auth("28accbe43ea112d9feb328d2c00b3eed", "");
   }
 
   private int createIssue(Issue newIssue) throws IOException {
